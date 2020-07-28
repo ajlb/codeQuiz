f5f8adb62eeceb6d1b1d638af6f47c2128765215
Nancy Lambert-Brown <2643160+n-lambert@users.noreply.github.com>
2020-07-28 00:19:58 -0700

    quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,84 @@
+//list all questions, choices, and answers
+var questions = [
+    {
+        title: "A benefit of including 'use strict' at the beginning of a JavaScript source file is:",
+        options: [
+            "it throws more verbose error messages", 
+            "makes your code case sensitive", 
+            "hides your script from developer tools", 
+            "disallows duplicate parameter values"
+        ],
+        answer: "disallows duplicate parameter values"
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        options: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "What is NaN",
+        options: [
+            "Not a Noun", 
+            "a camel-case variable", 
+            "Not a Number", 
+            "a type of bread"
+        ],
+        answer: "Not a Number"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "What is DOM?",
+        options: [
+            "a mafia guy", 
+            "disk operating model", 
+            "disk only memory", 
+            "document object model"
+        ],
+        answer: "document object model"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ____.",
+        options: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ],
+        answer: "all of the above"
+    },
+    {
+        title: "what is this?",
+        options: ["an object", "a function", "a nightmare", "a quiz"],
+        answer: "a function"
+    },
+    {
+        title:
+            "String values must be enclosed within ____ when being assigned to variables.",
+        options: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "How do you clone an object?",
+        options: ["copy paste", "set it to a variable", "use the assign method", "console.log it"],
+        answer: "use the assign method"
+    },
+    {
+        title: "what does push() do?",
+        options: [
+            "push something out of memory", 
+            "push an element onto an array", 
+            "shifts an alphabet array by one", 
+            "save something to memory"],
+        answer: "push an element onto an array"
+    },
+    {
+        title:
+            "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+    }
+]
